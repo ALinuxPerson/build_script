b009764a06e19fb8fd9cc3dc84067d117aacaa8a
ALinuxPerson <alinuxperson@gmail.com>
2021-02-06 21:44:42 +0800

    Add documentation for utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,13 @@
+//! Some other extra utility functions/traits that are only used internally within
+//! [`build_script`](crate).
+
+/// Some useful vector extensions.
 pub trait VecExt<T> {
+    /// Take ownership of an item. If the index is out of range, this returns [`None`](None).
     fn take(&mut self, index: usize) -> Option<T>;
+
+    /// Take ownership of the first item in a [`Vec`](Vec). If the vector is empty, this returns
+    /// [`None`](None).
     fn take_first(&mut self) -> Option<T>;
 }
 
