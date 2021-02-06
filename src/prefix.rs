28925ae16938d13403ccc74ba64a67b0a6c047ea
ALinuxPerson <alinuxperson@gmail.com>
2021-02-06 21:40:27 +0800

    Add documentation for prefix

--- src/prefix.rs
+++ src/prefix.rs
@@ -1,12 +1,18 @@
+//! This contains the [`Prefix`](Prefix) enum.
 use std::fmt;
 
+/// The prefix. Usually [`Cargo`](Self::Cargo).
 #[derive(Clone)]
 pub enum Prefix {
+    /// The cargo prefix. 99% of the time this is used.
     Cargo,
+
+    /// Other, custom prefixes.
     Custom(String),
 }
 
 impl Default for Prefix {
+    /// The default prefix is [`Cargo`](Self::Cargo).
     fn default() -> Self {
         Self::Cargo
     }
