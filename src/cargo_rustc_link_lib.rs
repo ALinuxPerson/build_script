60754b1fa18a2ced9a500a63fc0b597293b5b601
ALinuxPerson <alinuxperson@gmail.com>
2021-02-06 20:44:58 +0800

    Add documentation for cargo_rustc_link_lib

--- src/cargo_rustc_link_lib.rs
+++ src/cargo_rustc_link_lib.rs
@@ -1,12 +1,24 @@
+//! A wrapper for [`cargo_rustc_link_lib`](crate::BuildScript::cargo_rustc_link_lib).
+/// A kind for [`cargo_rustc_link_lib`](crate::BuildScript::cargo_rustc_link_lib).
 pub enum Kind {
+    /// Known to the compiler as [`dylib`](Self::DYNAMIC_LIBRARY).
     DynamicLibrary,
+
+    /// Known to the compiler as [`static`](Self::STATIC).
     Static,
+
+    /// Known to the compiler as [`framework`](Self::FRAMEWORK).
     Framework,
 }
 
 impl Kind {
+    /// Known to this library as [`DynamicLibrary`](Self::DynamicLibrary).
     pub const DYNAMIC_LIBRARY: &'static str = "dylib";
+
+    /// Known to this library as [`Static`](Self::Static).
     pub const STATIC: &'static str = "static";
+
+    /// Known to this library as [`Framework`](Self::Framework).
     pub const FRAMEWORK: &'static str = "framework";
 }
 
